a7aba5f161ee3a9e3d15cbb6d20c0c0b3c407c5a
John Ryan <uijedi@gmail.com>
2018-05-26 15:19:03 -0400

    add args parsing for filename

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -4,32 +4,37 @@ import java.io.InputStream
 
 fun main(args: Array<String>) {
 
-    val inputStream: InputStream  = File("test.txt").inputStream()
+    if (args.size == 0) {
+        println("You must provide a file name.")
+    } else {
+        val inputStream: InputStream  = File(args[0]).inputStream()
+        val input = inputStream.bufferedReader().use{ it.readText() }
+        val words = input.replace(",", "")
+                .replace(".", "")
+                .replace("!", "")
+                .replace("?", "")
+                .replace("\n", " ")
+                .split(" ")
+        val wordMap = mutableMapOf<String, Int>()
 
-    val input = inputStream.bufferedReader().use{ it.readText() }
-
-//    val input = "Hello there my name is Bob. I like to eat tacos, because I think they are great! bob bob Bob"
-
-    val words = input.replace(",", "")
-            .replace(".", "")
-            .replace("!", "")
-            .replace("?", "")
-            .replace("\n", " ")
-            .split(" ")
-    val wordMap = mutableMapOf<String, Int>()
-    for (word in words) {
-        if (wordMap[word.capitalize()] == null) {
-            wordMap[word.capitalize()] = 1
-        } else {
-            val wordCount = wordMap[word.capitalize()]!!
-            wordMap[word.capitalize()] = wordCount + 1
+        for (word in words) {
+            if (wordMap[word.capitalize()] == null) {
+                wordMap[word.capitalize()] = 1
+            } else {
+                val wordCount = wordMap[word.capitalize()]!!
+                wordMap[word.capitalize()] = wordCount + 1
+            }
         }
-    }
 
-    val wordList = wordMap.toList()
-    val sortedList = wordList.sortedWith(compareByDescending({it.second}))
-
-    for (word in sortedList) {
-        println("${word.first} = ${word.second}")
+        val wordList = wordMap.toList()
+        val sortedList = wordList.sortedWith(compareByDescending({it.second}))
+        var count = 0
+        for (word in sortedList) {
+            // only print top 10
+            if (count < 10) {
+                println("${word.first} = ${word.second}")
+            }
+            count += 1
+        }
     }
 }
